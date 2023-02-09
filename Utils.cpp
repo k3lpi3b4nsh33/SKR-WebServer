df1fbc67eabbf3ad60c4527c9ffabc3d31e7c680
k3lpi3b4nsh33 <kelpie@skiff.com>
2023-02-09 09:23:47 -0500

    update the comments

--- Utils.cpp
+++ Utils.cpp
@@ -13,40 +13,44 @@
 #include "MutexLock.h"
 #include "Utils.h"
 
+/**
+ *
+ * @brief The basic server functions in this file.
+ *
+ */
+
+
+
+// socket create
 int socket_bind_and_listen(int port)
 {
     int listen_fd = 0;
-    // 开始创建 socket, 注意这是阻塞模式的socket
     // AF_INET      : IPv4 Internet protocols  
     // SOCK_STREAM  : TCP socket
     if((listen_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, 0)) == -1)
         return -1;
 
-    // 绑定端口
     sockaddr_in server_addr;
-    // 初始化一下
     memset(&server_addr, '\0', sizeof(server_addr));
-    // 设置一下基本操作
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons((unsigned short)port);
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    // 端口复用
+
+
     int opt = 1;
     if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
         return -1;
-    // 试着bind
     if(bind(listen_fd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1)
         return -1;
-    // 试着listen, 设置最大队列长度为 1024
     if(listen(listen_fd, 1024) == -1)
         return -1;
 
     return listen_fd;
 }
 
+// set the flag in fd
 bool setFdNoBlock(int fd)
 {
-    // 获取fd对应的flag
     int flag = fcntl(fd, F_GETFD);
     if(flag == -1)
         return -1;
@@ -64,29 +68,31 @@ bool setSocketNoDelay(int fd)
     return true;
 }
 
+/**
+ * @brief read the file
+ * @param fd file descriptor
+ * @param buf data
+ * @param len the len of data
+ * @return
+ */
 ssize_t readn(int fd, void* buf, size_t len)
 {
-    // 这里将 void* 转换成 char* 是为了在下面进行自增操作
     char *pos = (char*)buf;
     size_t leftNum = len;
     ssize_t readNum = 0;
     while(leftNum > 0)
     {
-        // 尝试循环读取,如果报错,则进行判断
-        // 注意, read 的返回值为0则表示读取到 EOF,是正常现象
         ssize_t tmpRead = read(fd, pos, leftNum);
-
         if(tmpRead < 0)
         {
             if(errno == EINTR)
                 tmpRead = 0;
-                // 如果始终读取不到数据,则提前返回,因为这个取决于远程 fd,无法预测要等多久
             else if (errno == EAGAIN)
                 return readNum;
             else
                 return -1;
         }
-        // 读取的0,则说明远程连接已被关闭
+        // tmpread == 0 ,the connection is close
         if(tmpRead == 0)
             break;
         readNum += tmpRead;
@@ -97,9 +103,16 @@ ssize_t readn(int fd, void* buf, size_t len)
     return readNum;
 }
 
+/**
+ * @brief write something in file
+ * @param fd
+ * @param buf
+ * @param len
+ * @param isWrite
+ * @return
+ */
 ssize_t writen(int fd, const void* buf, size_t len, bool isWrite)
 {
-    // 这里将 void* 转换成 char* 是为了在下面进行自增操作
     char *pos = (char*)buf;
     size_t leftNum = len;
     ssize_t writtenNum = 0;
@@ -112,11 +125,8 @@ ssize_t writen(int fd, const void* buf, size_t len, bool isWrite)
         else
             tmpWrite = send(fd, pos, leftNum, 0);
 
-        // 尝试循环写入,如果报错,则进行判断
-        // 注意,write返回0属于异常现象,因此判断时需要包含
         if(tmpWrite < 0)
         {
-            // 与read不同的是,如果 EAGAIN,则继续重复写入,因为写入操作是有Server这边决定的
             if(errno == EINTR || errno == EAGAIN)
                 tmpWrite = 0;
             else
@@ -143,7 +153,6 @@ void handleSigpipe()
 
 void printConnectionStatus(int client_fd_, string prefix)
 {
-    // 输出连接信息 [Server]IP:PORT <---> [Client]IP:PORT
     sockaddr_in serverAddr, peerAddr;
     socklen_t serverAddrLen = sizeof(serverAddr);
     socklen_t peerAddrLen = sizeof(peerAddr);
@@ -161,14 +170,14 @@ void printConnectionStatus(int client_fd_, string prefix)
 string escapeStr(const string& str, size_t MAXBUF)
 {
     string msg = str;
-    // 遍历所有字符
+    // Traverse the string
     for(size_t i = 0; i < msg.length(); i++)
     {
         char ch = msg[i];
-        // 如果当前字符无法打印,则转义
+        // the char can't print
         if(!isprint(ch))
         {
-            // 这里只对\r\n做特殊处理
+            // Only process '\r', '\n'
             string substr;
             if(ch == '\r')
                 substr = "\\r";
@@ -177,14 +186,13 @@ string escapeStr(const string& str, size_t MAXBUF)
             else
             {
                 char hex[10];
-                // 注意这里要设置成 unsigned,即零扩展
                 snprintf(hex, 10, "\\x%02x", static_cast<unsigned char>(ch));
                 substr = hex;
             }
             msg.replace(i, 1, substr);
         }
     }
-    // 将读取到的数据输出
+    // output the read data
     if(msg.length() > MAXBUF)
         return msg.substr(0, MAXBUF) + " ... ... ";
     else
@@ -211,7 +219,6 @@ size_t closeRemainingConnect(int listen_fd, int* idle_fd) {
         close(client_fd);
         ++count;
     }
-    // 重新恢复空闲描述符
     *idle_fd = open("/dev/null", O_RDONLY | O_CLOEXEC);
     return count;
 }
@@ -243,18 +250,19 @@ bool is_path_parent(const string& parent_path, const string& child_path) {
     // INFO("resolved parent path: %s", parent_p);
     INFO("resolved path: %s", child_p);
 
-    /* 判断是否存在目录穿越漏洞，判断条件：
-        1. parent_path 是否在 child_path 的起始位置，
-            例如 parent: /usr/class/html
-            与   child:  /usr/class/html/index.html
-        2. 判断 parent_path 末尾是否分割符
-            例如 parent: /usr/class/html
-            与   child:  /usr/class/htmlflag/../../../../flag
-            ----------------------------A--------------------
-                      这里没有在 html 后面加 /，说明两个路径不对应
-    */
+    /**
+     * Determine whether there is a directory traversal vulnerability
+     *
+     * Normal visit:
+     *              parent: /usr/wwwroot/www/html
+     *              child: /usr/wwwroot/www/html/index.html
+     * Trick visit:
+     *              parent: /usr/wwwroot/www/html
+     *              child:  /usr/wwwroot/www/html/../../../../../../flag
+     */
+
     if(child_p == strstr(child_p, parent_p)) {
-        // parent 在 child 中，因此 child[parent.len] 不会越界
+        // The parent is in the child, so the child [parent. len] will not cross the boundary
         separator = child_p[strlen(parent_p)];
         if (separator == '\0' || separator == '/')
             return true;
